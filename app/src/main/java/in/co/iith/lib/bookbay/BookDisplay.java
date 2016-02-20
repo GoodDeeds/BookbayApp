ab3d30685f89d4bc95c6c5eb869609a9c76d0cce
Prateek Kumar <pksingh1023@gmail.com>
2016-02-21 01:40:36 +0530

    new act

---
+++ app/src/main/java/in/co/iith/lib/bookbay/BookDisplay.java
@@ -0,0 +1,13 @@
+package in.co.iith.lib.bookbay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_display);
+    }
+}
