10e4f5be3943a055c3e7684b77153edd2b12f532
GoodDeeds <raosukrut@gmail.com>
2016-02-22 23:05:59 +0530

    added download capability

--- app/src/main/java/in/co/iith/lib/bookbay/BookDetails.java
+++ app/src/main/java/in/co/iith/lib/bookbay/BookDetails.java
@@ -1,9 +1,13 @@
 package in.co.iith.lib.bookbay;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,12 +15,14 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -28,6 +34,8 @@ public class BookDetails extends AppCompatActivity {
 
     String bookId;
     JSONArray bookList;
+    String bookName;
+    String bookurl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,97 +72,98 @@ public class BookDetails extends AppCompatActivity {
 //            }
 //            query_string = "Search result for query \""+message+"\"";
             JSONObject loadedData = bookList.getJSONObject(0);
-            String[] resultString = new String[5];
-            String bookName;
+            String[] resultString = new String[6];
+
             String courseID;
             String bookAuthor;
-            String bookID;
+
             String description;
             resultString[1] = bookName = loadedData.getString("name");
             resultString[3] = courseID = loadedData.getString("course");
             resultString[2] = bookAuthor = loadedData.getString("author");
-            resultString[0] = bookID = loadedData.getString("id");
+            resultString[0] = bookId = loadedData.getString("id");
             resultString[4] = description = loadedData.getString("description");
+            resultString[5] = bookurl = loadedData.getString("url");
 
             //  resultString[i] = loadedData.getString("name");
 
 
 
 
-        return resultString;
-    }
+            return resultString;
+        }
 
-    @Override
-    protected String[] doInBackground(String... params) {
-        // These two need to be declared outside the try/catch
+        @Override
+        protected String[] doInBackground(String... params) {
+            // These two need to be declared outside the try/catch
 // so that they can be closed in the finally block.
-        HttpURLConnection urlConnection = null;
-        BufferedReader reader = null;
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
 
-        // Will contain the raw JSON response as a string.
-        String forecastJsonStr = null;
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
 
-        try {
-            // Construct the URL for the OpenWeatherMap query
-            // Possible parameters are available at OWM's forecast API page, at
-            // http://openweathermap.org/API#forecast
-            final String FORECAST_BASE_URL = "https://lib.iith.co.in/api/book?";
-            final String QUERY_PARAM = "id";
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are available at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                final String FORECAST_BASE_URL = "https://lib.iith.co.in/api/book?";
+                final String QUERY_PARAM = "id";
 
-            Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                    .appendQueryParameter(QUERY_PARAM, params[0])
-                    .build();
-            URL url = new URL(builtUri.toString());
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, params[0])
+                        .build();
+                URL url = new URL(builtUri.toString());
 
 
-            // Create the request to OpenWeatherMap, and open the connection
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
 
-            // Read the input stream into a String
-            InputStream inputStream = urlConnection.getInputStream();
-            StringBuffer buffer = new StringBuffer();
-            if (inputStream == null) {
-                // Nothing to do.
-                forecastJsonStr = null;
-            }
-            reader = new BufferedReader(new InputStreamReader(inputStream));
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    forecastJsonStr = null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                // But it does make debugging a *lot* easier if you print out the completed
-                // buffer for debugging.
-                buffer.append(line + "\n");
-            }
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
 
-            if (buffer.length() == 0) {
-                // Stream was empty.  No point in parsing.
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e("Bookbay", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attempting
+                // to parse it.
                 return null;
-            }
-            forecastJsonStr = buffer.toString();
-
-        } catch (IOException e) {
-            Log.e("Bookbay", "Error ", e);
-            // If the code didn't successfully get the weather data, there's no point in attempting
-            // to parse it.
-            return null;
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (reader != null) {
-                try {
-                    return getBookDataFromJSON(forecastJsonStr);
-                } catch (JSONException e) {
-                    Log.e("Bookbay", "Error closing stream", e);
-                    e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        return getBookDataFromJSON(forecastJsonStr);
+                    } catch (JSONException e) {
+                        Log.e("Bookbay", "Error closing stream", e);
+                        e.printStackTrace();
+                    }
                 }
             }
+            return null;
         }
-        return null;
-    }
         @Override
         protected void onPostExecute(String[] result) {
             if(result!=null){
@@ -170,7 +179,198 @@ public class BookDetails extends AppCompatActivity {
 
         }
 
-}
+    }
+    public void download(View view) {
+
+        String url = bookurl; //"https://lib.iith.co.in/u/api/book/download/" + bookId + "/" + bookName + ".pdf";
+
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setDescription("Downloading...");
+        request.setTitle(bookName);
+// in order for this if to run, you must use the android 3.2 to compile your app
+        // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+        request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        // }
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, bookName + ".pdf");
+
+        // get download service and enqueue file
+        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
+    }
+//    public void download()
+//    {
+////        Intent intent = new Intent(getApplicationContext(),DownloadBook.class);
+////        intent.putExtra("Download",bookId);
+////        if(intent.resolveActivity(getPackageManager())!=null)
+////        {
+////            startActivity(intent);
+////        }
+//        DownloadBook downloadBook = new DownloadBook();
+//        DownloadBook.execute();
+//    }
+//
+//    public class DownloadBook extends AsyncTask<String, Void, String> {
+//        String bookID;
+//        String bookName;
+//
+//        public DownloadBook()
+//        {
+//
+//
+//        }
+//        public String[] getBookDataFromJSON(String bookJSONObject) throws JSONException {
+//
+////            if(bookList.length()==0)
+////            {
+////                String[] resultString = new String[0];
+////                // resultString[0] = "No results found";
+////                query_string = "No books found for query \""+message+"\"";
+////                return resultString;
+////            }
+////            query_string = "Search result for query \""+message+"\"";
+//            JSONObject loadedData = bookList.getJSONObject(0);
+//            String[] resultString = new String[5];
+//
+//            String courseID;
+//            String bookAuthor;
+//
+//            String description;
+//            resultString[1] = bookName = loadedData.getString("name");
+//            resultString[3] = courseID = loadedData.getString("course");
+//            resultString[2] = bookAuthor = loadedData.getString("author");
+//            resultString[0] = bookID = loadedData.getString("id");
+//            resultString[4] = description = loadedData.getString("description");
+//
+//            //  resultString[i] = loadedData.getString("name");
+//
+//
+//
+//
+//            return resultString;
+//        }
+//
+//        @Override
+//        protected String doInBackground(String... params) {
+//            // These two need to be declared outside the try/catch
+//// so that they can be closed in the finally block.
+//            JSONObject loadedData = null;
+//            try {
+//                loadedData = bookList.getJSONObject(0);
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            String[] resultString = new String[5];
+//
+//            String courseID;
+//            String bookAuthor;
+//
+//            String description;
+//            try {
+//                resultString[1] = bookName = loadedData.getString("name");
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            try {
+//                resultString[3] = courseID = loadedData.getString("course");
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            try {
+//                resultString[2] = bookAuthor = loadedData.getString("author");
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            try {
+//                resultString[0] = bookID = loadedData.getString("id");
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            try {
+//                resultString[4] = description = loadedData.getString("description");
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//
+//            HttpURLConnection urlConnection = null;
+//            BufferedReader reader = null;
+//
+//            // Will contain the raw JSON response as a string.
+//            String forecastJsonStr = null;
+//
+//            try {
+//                // Construct the URL for the OpenWeatherMap query
+//                // Possible parameters are available at OWM's forecast API page, at
+//                // http://openweathermap.org/API#forecast
+//                final String FORECAST_BASE_URL = "https://lib.iith.co.in/api/book?";
+//                final String QUERY_PARAM = "id";
+//
+//                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+//                        .appendQueryParameter(QUERY_PARAM, params[0])
+//                        .build();
+//                URL url = new URL("https://lib.iith.co.in/u/book/download/"+bookId+"/"+bookName+".pdf");
+//
+//
+//                // Create the request to OpenWeatherMap, and open the connection
+//                urlConnection = (HttpURLConnection) url.openConnection();
+//                urlConnection.setRequestMethod("GET");
+//                urlConnection.connect();
+//
+//                // Read the input stream into a String
+//                InputStream inputStream = urlConnection.getInputStream();
+//                StringBuffer buffer = new StringBuffer();
+//                if (inputStream == null) {
+//                    // Nothing to do.
+//                    forecastJsonStr = null;
+//                }
+//                reader = new BufferedReader(new InputStreamReader(inputStream));
+//
+//                String line;
+//                while ((line = reader.readLine()) != null) {
+//                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+//                    // But it does make debugging a *lot* easier if you print out the completed
+//                    // buffer for debugging.
+//                    buffer.append(line + "\n");
+//                }
+//
+//                if (buffer.length() == 0) {
+//                    // Stream was empty.  No point in parsing.
+//                    return null;
+//                }
+//                forecastJsonStr = buffer.toString();
+//                FileOutputStream output = new FileOutputStream("/sdcard/"+bookName+".pdf");
+//
+//            } catch (IOException e) {
+//                Log.e("Bookbay", "Error ", e);
+//                // If the code didn't successfully get the weather data, there's no point in attempting
+//                // to parse it.
+//                return null;
+//            } finally {
+//                if (urlConnection != null) {
+//                    urlConnection.disconnect();
+//                }
+//                if (reader != null) {
+//                    return "Done";
+//                }
+//            }
+//            return null;
+//        }
+//        @Override
+//        protected void onPostExecute(String result) {
+//            if(result!=null){
+//                Toast.makeText(getApplicationContext(),"File Downloaded",Toast.LENGTH_SHORT);
+//
+//
+//
+//            }
+//            else
+//            {
+//                Toast.makeText(getApplicationContext(),"Error downloading file",Toast.LENGTH_SHORT);
+//            }
+//
+//        }
+//
+//    }
 
 
     /*public void onClick(View v) {
